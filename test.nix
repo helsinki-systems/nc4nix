8a3015bd8a4057a6338a1a691f87dc410cf6ae3a
ajs124 <git@ajs124.de>
2022-11-02 15:15:34 +0100

    test: add more plugins
    - build for 24, instead of 23
    - attrset instead of list

--- test.nix
+++ test.nix
@@ -1,7 +1,10 @@
 { pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
 let
   nc4nix = pkgs.callPackage ./. {};
-in
-  (with nc4nix."23"; [
+in {
+  inherit (nc4nix."24")
+    breezedark
+    drawio
     groupfolders
-  ])
+    onlyoffice;
+}
