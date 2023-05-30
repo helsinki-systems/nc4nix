a27ac4bdfa5384f26b99ff4c9cca95db69f6fa89
ajs124 <git@ajs124.de>
2023-05-30 15:50:35 +0200

    test: build apps for supported versions

--- test.nix
+++ test.nix
@@ -1,10 +1,12 @@
 { pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
 let
   nc4nix = pkgs.callPackage ./. {};
-in {
-  inherit (nc4nix."24")
-    breezedark
-    drawio
-    groupfolders
-    onlyoffice;
-}
+  ncVersions = map lib.versions.major (import ./nc-versions.nix { inherit pkgs; }).n;
+in builtins.map (v: lib.recurseIntoAttrs {
+    inherit (nc4nix."${v}")
+      breezedark
+      drawio
+      groupfolders
+      onlyoffice;
+  }
+) ncVersions
