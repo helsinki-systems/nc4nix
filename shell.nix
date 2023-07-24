7d1f17767b8b9469b71536354328c2043c76fe79
Matthieu Coudron <886074+teto@users.noreply.github.com>
2023-07-24 23:22:46 +0200

    feat: adopt flakes

--- shell.nix
+++ shell.nix
@@ -1,14 +1,22 @@
-with import <nixpkgs> { };
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+}:
 
-stdenv.mkDerivation {
-  name = "go";
-  buildInputs = [
-    delve
-    libcap
-    go
-    gcc
+let
+  goEnv = pkgs.mkGoEnv { pwd = ./.; };
+in
+pkgs.mkShell {
+  packages = [
+    goEnv
+    pkgs.gomod2nix
   ];
-  shellHook = ''
-    export GOPATH=$PWD/gopath
-  '';
 }
