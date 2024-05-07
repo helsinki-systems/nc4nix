ad014e3ea7fba9d1e7f9c6947825937463d53aff
Janne Heß <janne@hess.ooo>
2024-05-07 17:42:38 +0200

    Don't check for the groupfolders plugin for now

--- test.nix
+++ test.nix
@@ -4,7 +4,7 @@ let
   ncVersions = map lib.versions.major (import ./nc-versions.nix { inherit pkgs; }).n;
 in builtins.map (v: lib.recurseIntoAttrs {
     inherit (nc4nix."nextcloud-${v}")
-      groupfolders
+      # groupfolders FIXME: https://github.com/nextcloud/groupfolders/issues/2940
       onlyoffice
       spreed   # aka talk
       twofactor_webauthn
