0949c18a8b0789c46c877fca41fa9c46792cde23
Janne Heß <janne@hess.ooo>
2025-10-27 12:00:07 +0100

    Find nc versions from unstable

--- nc-versions.nix
+++ nc-versions.nix
@@ -1,5 +1,5 @@
 # this file is used to figure out which versions of nextcloud we have in nixpkgs
-{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
+{ pkgs ? import <unstable> {}, lib ? pkgs.lib }:
 let
   n = lib.mapAttrsToList (_: v: v.version) (
       lib.filterAttrs (k: v: builtins.match "nextcloud[0-9]+" k != null && (builtins.tryEval v.version).success)
